25e2f081216e06c4399f21204601c7ea3cdff3f1
xyz <xyz@xyz.com>
2018-08-16 10:45:20 +0530

    Add project files.

---
+++ sprint1 testing/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="sprint1_testing.MvcApplication" Language="C#" %>
---
+++ sprint1 testing/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(sprint1_testing.Startup))]
+namespace sprint1_testing
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
